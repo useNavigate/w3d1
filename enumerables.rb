0fa4c7e2ae09519b5d1f9909e5387b1945a33623
sara <041416sara@gmail.com>
2023-01-23 17:04:24 -0500

    my_select

--- enumerables.rb
+++ enumerables.rb
@@ -9,18 +9,30 @@ class Array
     end
     self
   end
+
+def my_select(&block)
+  res=[]
+  self.my_each{|el| res << el if block.call(el)}
+    res
+  end
+
 end
 
-return_value = [1, 2, 3].my_each do |num|
-  puts num
-end.my_each do |num|
-  puts num
-end
-# => 1
-2
-3
-1
-2
-3
 
-p return_value  # => [1, 2, 3]
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
+
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+# # => 1
+# 2
+# 3
+# 1
+# 2
+# 3
+
+# p return_value  # => [1, 2, 3]
